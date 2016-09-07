570f1aed6a534c7415f9271f2e5c3db37615924e
StarSkyLight <stark_09@foxmail.com>
2016-09-07 21:37:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ziyi/implicitintentpractice/AnotherActivity.java
@@ -0,0 +1,16 @@
+package com.example.ziyi.implicitintentpractice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ziyi on 2016/9/7.
+ */
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+    }
+}
